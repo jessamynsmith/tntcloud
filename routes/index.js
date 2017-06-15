2213d7a9dc537c85a6840972d6a0670c5869d865
NEH <erik@bodigiti.com>
2017-06-15 11:50:52 -0400

    Added monk and code through CRUD along with video, but when I use the 'GET' link on the site, it returns 'data.on is not a function.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var mongo = require('mongodb').MongoClient;
-// for Update
-var objectId = require('mongodb').ObjectID;
-// mongo support module
-var assert = require('assert');
-
-var url = 'mongodb://localhost:27017/test';
+// get database connection
+var db = require('monk')('localhost:27017/test');
+// get the collection 'user-data'... setup data object
+var userData = db.get('user-data');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,19 +12,12 @@ router.get('/', function(req, res, next) {
 });
 // Get Data
 router.get('/get-data', function(req, res, next) {
-  var resultArray = [];
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    // entries stored in the cursor variable
-    var cursor = db.collection('user-data').find();
-    cursor.forEach(function(doc, err) {
-      assert.equal(null, err);
-      resultArray.push(doc);
-    }, function() {
-      db.close();
-      // render items on the index page
-      res.render('index', {items: resultArray});
-    });
+  // pass empty object { } to get all data in the collection
+  var data = userData.find({});
+  data.on('success', function(docs) {
+    // pass the results to our 'index' view ...render
+    // i could bind the database query to variable which is then promise...
+    res.render('index', {items: docs});
   });
 });
 // Insert Data
@@ -39,16 +29,8 @@ router.post('/insert', function(req, res, next) {
     author: req.body.author
   };
   // insert
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    // access the db collection into which you want to insert data
-    // 'insertOne' used to insert one entry
-    db.collection('user-data').insertOne(item, function(err, result) {
-      assert.equal(null, err);
-      console.log("Item inserted");
-      db.close();
-    });
-  });
+  // i could bind the database query to variable which is then promise...
+  userData.insert(item);
   // url redirect after post
   res.redirect('/');
 });
@@ -63,32 +45,18 @@ router.post('/update', function(req, res, next) {
   // identify id of item to update
   var id = req.body.id;
   // update
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    // access the db collection into which you want to insert data
-    // 'insertOne' used to insert one entry ... replace 'objectId' with {$set: item}
-    db.collection('user-data').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
-      assert.equal(null, err);
-      console.log("Item updated");
-      db.close();
-    });
-  });
+  // method #1: userData.update({"_id", db.id(id)}, item);
+  // i could bind the database query to variable which is then promise...
+  userData.updateById(id, item);
 });
 // Delete Data
 router.post('/delete', function(req, res, next) {
   // identify id of item to delete
   var id = req.body.id;
   // delete
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    // access the db collection into which you want to insert data
-    // 'insertOne' used to insert one entry ... replace 'objectId' with {$set: item}
-    db.collection('user-data').deleteOne({"_id": objectId(id)}, function(err, result) {
-      assert.equal(null, err);
-      console.log("Item deleted");
-      db.close();
-    });
-  });
+  // method #1 userData.delete({"_id": db.id(id)});
+  // i could bind the database query to variable which is then promise...
+  userData.removeById(id);
 });
 
 module.exports = router;
