eafd47de4355f20f90546bff9005a2693873ea69
NEH <erik@bodigiti.com>
2017-06-15 14:01:41 -0400

    Update works with manual entry for 'title' in code, and then using variable to post the updates from the form for 3 of the 4 fields: Title, Content, Author.

--- routes/index.js
+++ routes/index.js
@@ -44,11 +44,10 @@ router.post('/update', function(req, res, next) {
   };
   // identify id of item to update
 //  var id = req.body.id;
-var titleNew = 'whats up';
   // update
   // method #1: userData.update({"_id", db.id(id)}, item);
   // i could bind the database query to variable which is then promise...
-  userData.update({title: 'nnnnnnnnnn'}, {title: titleNew, content: 'okay', author: 'okay'});
+  userData.update({title: 'okay'}, item);
 });
 // Delete Data
 router.post('/delete', function(req, res, next) {
