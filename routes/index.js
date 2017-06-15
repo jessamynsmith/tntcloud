9e1c8a34cbef9716ebf4911a4bd0d2fae21d883d
NEH <erik@bodigiti.com>
2017-06-15 08:35:17 -0400

    Insert data and Get data.

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,65 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+// mongo support module
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
+  res.render('index');
   req.session.errors = null;
 });
 
-// validation
-router.post('/submit', function(req, res, next) {
-  // Check validity
-  req.check('email', 'Invalid email address').isEmail();
-  // Min lenght 4 characters, and validate the password = confirm password field
-  req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword);
+router.get('/get-data', function(req, res, next) {
+  var resultArray = [];
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    // entries stored in the cursor variable
+    var cursor = db.collection('user-data').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      // render items on the index page
+      res.render('index', {items: resultArray});
+    });
+  });
+});
 
-  var errors = req.validationErrors();
-  if (errors) {
-    req.session.errors = errors;
-    req.session.success = false;
-  } else {
-    req.session.success = true;
-  }
+router.post('/insert', function(req, res, next) {
+  // get the form fields data
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+
+  // insert
+  mongo.connect(url, function(err, db) {
+    assert.equal(null, err);
+    // access the db collection into which you want to insert data
+    // 'insertOne' used to insert one entry
+    db.collection('user-data').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log("Item inserted");
+      db.close();
+    });
+  });
+
+  // url redirect after post
   res.redirect('/');
 });
+
+router.post('/update', function(req, res, next) {
+
+});
+
+router.post('/delete', function(req, res, next) {
+
+});
+
 
 module.exports = router;
