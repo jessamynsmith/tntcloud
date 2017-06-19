a40172dffb653303680863a784563bb0853d86e0
NEH <erik@bodigiti.com>
2017-06-19 18:55:07 -0400

    Misc.

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var firebase = require('firebase');
-var admin = require('../firebase-admin-init')
-
 /*******************************************************************************
  * Firebase Initialize
  ******************************************************************************/
+ var firebase = require("../firebase");
+
  var dbRef = firebase.database().ref('node');
 
- var usersRef = dbRef.child('users');
+ var warrantyRef = dbRef.child('warranty');
+// var usersRef = dbRef.child('users');
 /** Firebase End **************************************************************/
+
+var admin = require('../firebase-admin-init')
+
 
 
 /* GET users listing. */
@@ -25,10 +28,25 @@ router.get('/user-create', function(req, res, next) {
 router.get('/user-create-input', function(req, res, next) {
   res.send('yup ');
 });
-
+/*
+// Insert Data
+router.post('/user-create-input', function(req, res, next) {
+  // get the form fields data
+  var item = {
+  	email: req.body.email,
+  	pass: req.body.password
+  };
+  // insert
+  // i could bind the database query to variable which is then promise...
+  warrantyRef.push(item);
+  // url redirect after post
+  res.redirect('/core-warranty');
+});
+*/
 /*******************************************************************************
  * Login
  ******************************************************************************/
+/*
 router.post('/user-create-input', function(req, res){
 
   var item = {
@@ -44,15 +62,11 @@ router.post('/user-create-input', function(req, res){
   .catch(function(error) {
     console.log("Error creating new user:", error);
   });
-
-  // insert
   // i could bind the database query to variable which is then promise...
-//  warrantyRef.push(item);
   // url redirect after post
   res.redirect('user-create');
 
-
 });
-
+*/
 
 module.exports = router;
