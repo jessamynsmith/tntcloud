f92e3d4a28faaf5a17543075e022790048052461
NEH <erik@bodigiti.com>
2017-06-22 10:34:01 -0400

    firebase-user.js: more code cleanup. App roles still function properly.

--- firebase-user.js
+++ firebase-user.js
@@ -11,32 +11,33 @@ var userRole = '';
 firebase.auth().onAuthStateChanged(function(user) {
   // Get 'user' from onAuthStateChanged and set to above fbUser
   fbUser = user;
-  if (user) {
-    console.log("firebase-user.js ", fbUser.uid);
-  } else {
-    // If there is no user, clear the role
+  // If there is no user, clear the role, for security purposes so roles do not get switched up between sessions
+  if (!user) {
     userRole = '';
   }
 });
-/******************************************************************************/
 
+/*******************************************************************************
+ * Get User Role Realtime Database User Role Value
+ ******************************************************************************/
 module.exports.getRole = function() {
   return new promise(function (resolve, reject) {
+    // userRole is empty, and empty strings are FALSE
+    // checking if userRole because getRole() could have already been called and gotten Role using below dfRef fb stuff....
+    // we don't want to continue to use the below code for round trips to firebase if unnecessary, because it slows things down
     if (userRole) {
       resolve(userRole);
     }
     // This case should rarely be hit, as the onAuthStateChanged handles populating fbUser
     // https://firebase.google.com/docs/auth/web/manage-users#get_the_currently_signed-in_user
-    if (!fbUser.uid) {
-      user = firebase.auth().currentUser;
-      if (user) {
-        fbUser = user;
-      }
+    if (!fbUser) {
+      fbUser = firebase.auth().currentUser;
     }
 
     // If there is still no user (could be not logged in or still initializing)
-    if (!fbUser.uid) {
-      resolve('');
+    if (!fbUser) {
+      userRole = '';
+      resolve(userRole);
     }
 
     dbRef.child('/users/' + fbUser.uid + '/role').once('value').then(function(snapshot) {
@@ -49,6 +50,8 @@ module.exports.getRole = function() {
   });
 }
 
+// Export function that contains the whole firebase user
+// Need to export a function, not just a variable because I need the value to be updated
 module.exports.getUser = function () {
   return fbUser;
 }
