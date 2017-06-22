4ce04f0748f6324a471b740b4a51b20f666f60e4
NEH <erik@bodigiti.com>
2017-06-22 18:17:00 -0400

    Add script from core/warranty app list-warranty to the list-warranty route. Comment hbs code from core/warranty app.  Misc. App still working, not crashed.

--- routes/core-warranty.js
+++ routes/core-warranty.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require("firebase");
 var mw = require('../middleware');
-
+var dbRef = firebase.database().ref();
 /*******************************************************************************
  * Core Warranty Home
  ******************************************************************************/
@@ -69,6 +70,61 @@ router.post('/insert-core', function(req, res, next) {
 router.get('/list-warranty', mw.userRole, function(req, res, next) {
   // works as boolean, if conditional is true, then true, conditional is false, then false
   var isAdmin = req.app.locals.userRole === 'admin';
+
+  /*****************************************************************************
+   * Data for Handlebars + Output HTML for Handlebars
+  *****************************************************************************/
+  // 'on' is firebase method for keeping live data, and 'value' is saying you want values
+  dbRef.once('value', gotData, errData);
+
+  function gotData(data) {
+    // access data values
+    var context = data.val();
+
+    /***************************************
+    * Handlebars
+    ***************************************/
+/*
+    var rawTemplate = document.getElementById("rowsTemplate").innerHTML;
+    // Node.js is needed if I want to pre-compile templates
+    var compiledTemplate = Handlebars.compile(rawTemplate);
+
+    // pass the array data values
+    var html = compiledTemplate(context);
+    // add html output to ID
+    document.getElementById('records-list').innerHTML += html;
+
+    /***************************************
+    * Sort initial table by Date
+    ***************************************/
+    // http://jsfiddle.net/UdvDD/
+    var $wrapper = $('.list');
+
+    $wrapper.find('.data-row').sort(function (a, b) {
+      return b.dataset.sort - a.dataset.sort;
+    })
+    .appendTo( $wrapper );
+
+    /***************************************
+    * LIST JS = Search and Columns Sort
+    ***************************************/
+    // <th> 'data-sort' value and <td> 'class' value must match to work
+    /* DATE SORT required work-around:
+     * (because of Date Turned In format DD/MM/YYY, which would work properly if it was YYYY/MM/DD)
+     * The column "Date Turned In" contains data-sort="w-date-server-turned-in"
+     * that actually sorts by the DateTimeStampServer column, which is hidden. */
+    var options = {
+      valueNames: [ 'w-date-server-turned-in', 'w-customer', 'w-description', 'w-failed-part-number', 'w-quantity', 'w-received-by', 'w-ro', 'w-turned-in-by', 'w-vendor', 'w-vin' ]
+    };
+    var monkeyList = new List('warranty', options);
+
+  } // End gotData function (sort needs to be included in this function)
+
+  function errData(err) {
+    console.log('Error!');
+    console.log(err);
+  }
+
 
   res.render('core-warranty/list-warranty', { isAdmin: isAdmin });
 });
