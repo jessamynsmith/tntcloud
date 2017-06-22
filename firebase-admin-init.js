f437f4c8bcaff7cf0fe239c3050bf78c787f69ef
NEH <erik@bodigiti.com>
2017-06-22 13:02:52 -0400

    Change Firebase Project FROM 'TNT Dispatch' TO 'TNT Cloud'.

--- firebase-admin-init.js
+++ firebase-admin-init.js
@@ -4,11 +4,11 @@
 var firebase = require("firebase");
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./tnt-dispatch-firebase-adminsdk-wdp26-a9969d5abc.json");
+var serviceAccount = require("./tnt-cloud-5d440-firebase-adminsdk-4aa5y-e87b4483a9.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://tnt-dispatch.firebaseio.com"
+  databaseURL: "https://tnt-cloud-5d440.firebaseio.com"
 });
 
 module.exports = admin;
