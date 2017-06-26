5756b1c12a2c3bd0f5911625213578e36d730f67
NEH <erik@bodigiti.com>
2017-06-26 17:44:35 -0400

    app.js: pasted app.js from branch 07-Core-Warranty---Works...

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.locals.firebase = require("./firebase");
 app.locals.dbRef = app.locals.firebase.database().ref();
 
 // Middleware must be required after ./firebase because middleware uses it
-// var mw = require('./middleware');
+var mw = require('./middleware');
 
 // import route files
 var index = require('./routes/index');
@@ -49,23 +49,21 @@ app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false})
 // define routes and route resource files -see @routes files
 app.use('/', index);
 app.use('/dispatch', /*mw.loggedIn,*/ dispatch);
-app.use('/dispatch-requests', /*mw.loggedIn,*/ dispatch);
-app.use('/dispatching', /*mw.loggedIn,*/ dispatch);
+// app.use('/dispatch-requests', /*mw.loggedIn,*/ dispatch);
+// app.use('/dispatching', /*mw.loggedIn,*/ dispatch);
 app.use('/core-warranty', /*mw.loggedIn,*/ coreWarranty);
-app.use('/create-warranty', /*mw.loggedIn,*/ coreWarranty);
-app.use('/create-core', /*mw.loggedIn,*/ coreWarranty);
-app.use('/people-list', /*mw.loggedIn,*/ coreWarranty);
-app.use('/people-add', /*mw.loggedIn,*/ coreWarranty);
-app.use('/people-delete', /*mw.loggedIn,*/ coreWarranty);
+//app.use('/create-warranty', /*mw.loggedIn,*/ coreWarranty);
+//app.use('/create-core', /*mw.loggedIn,*/ coreWarranty);
+//app.use('/people-list', /*mw.loggedIn,*/ coreWarranty);
+//app.use('/people-add', /*mw.loggedIn,*/ coreWarranty);
+//app.use('/people-delete', /*mw.loggedIn,*/ coreWarranty);
 app.use('/users', /*mw.loggedIn, mw.userRoleAndAdmin,*/ users);
-app.use('/user-create', /*mw.loggedIn, mw.userRoleAndAdmin,*/ users);
-app.use('/user-create-input', /*mw.loggedIn, mw.userRoleAndAdmin,*/ users);
-app.use('/user-edit', /*mw.loggedIn, mw.userRoleAndAdmin,*/ users);
-app.use('/user-edit-password', /*mw.loggedIn, mw.userRoleAndAdmin,*/ users);
-app.use('/delete-success', /*mw.loggedIn, mw.userRoleAndAdmin,*/ users);
+// app.use('/user-create', /*mw.loggedIn, mw.userRoleAndAdmin,*/ users);
+// app.use('/user-create-input', /*mw.loggedIn, mw.userRoleAndAdmin,*/ users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  console.log("app error 1");
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -73,13 +71,15 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+
+  console.log("app error 2");
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+//  res.render('error');
 });
 
 module.exports = app;
