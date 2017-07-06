d8beec64baa8e81badd5aec4679b8bf59b0353e2
NEH <erik@bodigiti.com>
2017-07-06 14:27:22 -0400

    request-form.js: Create Request form now loads with No 'Undefined' values. BUTgit add --all! CREATE REQUEST FORM SUBMIT is NOT WORKING.

--- public/javascripts/dispatch/request-form.js
+++ public/javascripts/dispatch/request-form.js
@@ -16,9 +16,9 @@ function requestFormLoad(clicked_title, clicked_id) {
   // Retrieve Dispatch Request Record
   //////////////////////////////////////////////////////////////////////////////
 
-  const key = clicked_id; // Get key passed to URL from 'view' link
+  var key = clicked_id; // Get key passed to URL from 'view' link
   // Use "KEY" url parameter to target the core/ record
-  let dbRefEdit = firebase.database().ref().child('dispatch/' + key);
+  var dbRefEdit = firebase.database().ref().child('dispatch/' + key);
   // Re: "once" https://firebase.google.com/docs/reference/js/firebase.database.Query#once
   dbRefEdit.once('value', gotData);
 
@@ -34,98 +34,98 @@ function requestFormLoad(clicked_title, clicked_id) {
     if (key) {
       var keyTitle = `<h6>${clicked_id}</h6>`;
       var { BranchFrom, BranchTo, Driver, Instructions, Reference, Status, Urgency, Vendor } = editRecord;
+      console.log("Edit Record ", editRecord);
     } else {
-      keyTitle = "";
-      var { BranchFrom, BranchTo, Driver, Instructions, Reference, Status, Urgency, Vendor } = "";
+      keyTitle = ""; BranchFrom = ""; BranchTo = ""; Driver = ""; Instructions = ""; Reference = "", Status = "", Urgency = "", Vendor = "";
     }
 
     console.log("Vendor? ", Vendor);
     // Assign core Data to Table <div>'s
 
-  /***************************************
-  * Request Input Form
-  ***************************************/
-  var requestInputForm =
-  `<div class="hover-form">
-    <div class="card">
-      <div class="card-divider">
-        ${requestCardTitle}
-        ${keyTitle}
-        <button class="close-button" data-close aria-label="Close modal" type="button">
-          <span aria-hidden="true">&times;</span>
-        </button>
+    /***************************************
+    * Request Input Form
+    ***************************************/
+    var requestInputForm =
+    `<div class="hover-form">
+      <div class="card">
+        <div class="card-divider">
+          ${requestCardTitle}
+          ${keyTitle}
+          <button class="close-button" data-close aria-label="Close modal" type="button">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      </div>
+      <!-- Vendor ------------------------------------------------------------->
+      <div class="input-group">
+        <span class="input-group-label">Vendor</span>
+        <input type="text" id="vendor" placeholder="Vendor" class="input-group-field" name="vendor" value="${Vendor}">
+      </div>
+      <!-- Branch From -------------------------------------------------------->
+      <div class="input-group">
+        <span class="input-group-label">From</span>
+        <select id="branch-from"  class="input-group-field" name="branch-from" value="${BranchFrom}">
+          <!-- <option value="">Select Branch</option> -->
+          <option value="JAX">JAX</option>
+          <option value="NFWS">NFWS</option>
+          <option value="LC">LC</option>
+          <option value="WC">WC</option>
+          <option value="440">440</option>
+        </select>
+      </div>
+      <!-- Branch To ---------------------------------------------------------->
+      <div class="input-group">
+        <span class="input-group-label">To</span>
+        <select id="branch-to"  class="input-group-field" name="branch-to" value="${BranchTo}">
+          <!-- <option value="">Select Branch</option> -->
+          <option value="JAX">JAX</option>
+          <option value="NFWS">NFWS</option>
+          <option value="LC">LC</option>
+          <option value="WC">WC</option>
+          <option value="440">440</option>
+        </select>
+      </div>
+      <!-- Priority ---------------------------------------------------------->
+      <div class="input-group">
+        <span class="input-group-label">Urgency</span>
+        <select id="urgency"  class="input-group-field" name="urgency" value="${Urgency}">
+          <option value="normal">Normal</option>
+          <option value="low">Low</option>
+          <option value="high">High</option>
+          <option value="urgent">Urgent</option>
+        </select>
+      </div>
+      <!-- Reference ---------------------------------------------------------->
+      <div class="input-group">
+        <span class="input-group-label">Reference</span>
+        <input type="text" id="reference" placeholder="Reference"  class="input-group-field" name="reference" value="${Reference}"/>
+      </div>
+      <!-- Instructions ------------------------------------------------------->
+      <div class="input-group">
+        <span class="input-group-label">Instructions</span>
+        <input type="text" id="instructions" placeholder="Instructions"  class="input-group-field" name="instructions" value="${Instructions}"/>
+      </div>
+      <!-- Driver ------------------------------------------------------->
+      <div class="input-group">
+        <span class="input-group-label">Driver</span>
+        <input type="text" id="driver" placeholder="Driver"  class="input-group-field" name="driver" value="${Driver}"/>
+      </div>
+      <!-- Submit Button ------------------------------------------------------>
+      <div>
+        <input id="submit-dispatch-request" type="submit" class="button primary">
       </div>
     </div>
-    <!-- Vendor ------------------------------------------------------------->
-    <div class="input-group">
-      <span class="input-group-label">Vendor</span>
-      <input type="text" id="vendor" placeholder="Vendor" class="input-group-field" name="vendor" value="${Vendor}">
-    </div>
-    <!-- Branch From -------------------------------------------------------->
-    <div class="input-group">
-      <span class="input-group-label">From</span>
-      <select id="branch-from"  class="input-group-field" name="branch-from" value="${BranchFrom}">
-        <!-- <option value="">Select Branch</option> -->
-        <option value="JAX">JAX</option>
-        <option value="NFWS">NFWS</option>
-        <option value="LC">LC</option>
-        <option value="WC">WC</option>
-        <option value="440">440</option>
-      </select>
-    </div>
-    <!-- Branch To ---------------------------------------------------------->
-    <div class="input-group">
-      <span class="input-group-label">To</span>
-      <select id="branch-to"  class="input-group-field" name="branch-to" value="${BranchTo}">
-        <!-- <option value="">Select Branch</option> -->
-        <option value="JAX">JAX</option>
-        <option value="NFWS">NFWS</option>
-        <option value="LC">LC</option>
-        <option value="WC">WC</option>
-        <option value="440">440</option>
-      </select>
-    </div>
-    <!-- Priority ---------------------------------------------------------->
-    <div class="input-group">
-      <span class="input-group-label">Urgency</span>
-      <select id="urgency"  class="input-group-field" name="urgency" value="${Urgency}">
-        <option value="normal">Normal</option>
-        <option value="low">Low</option>
-        <option value="high">High</option>
-        <option value="urgent">Urgent</option>
-      </select>
-    </div>
-    <!-- Reference ---------------------------------------------------------->
-    <div class="input-group">
-      <span class="input-group-label">Reference</span>
-      <input type="text" id="reference" placeholder="Reference"  class="input-group-field" name="reference" value="${Reference}"/>
-    </div>
-    <!-- Instructions ------------------------------------------------------->
-    <div class="input-group">
-      <span class="input-group-label">Instructions</span>
-      <input type="text" id="instructions" placeholder="Instructions"  class="input-group-field" name="instructions" value="${Instructions}"/>
-    </div>
-    <!-- Driver ------------------------------------------------------->
-    <div class="input-group">
-      <span class="input-group-label">Driver</span>
-      <input type="text" id="driver" placeholder="Driver"  class="input-group-field" name="driver" value="${Driver}"/>
-    </div>
-    <!-- Submit Button ------------------------------------------------------>
-    <div>
-      <input id="submit-dispatch-request" type="submit" class="button primary">
-    </div>
-  </div>
-  `
-  /*******************************************************************************
-   * Request Form Add to DOM
-  *******************************************************************************/
-  // Request Create
-  var parentDivRequestCreate = document.getElementById("requestCreateForm");
-  parentDivRequestCreate.innerHTML = requestInputForm;
+    `
+    /*******************************************************************************
+     * Request Form Add to DOM
+    *******************************************************************************/
+    // Request Create
+    var parentDivRequestCreate = document.getElementById("requestCreateForm");
+    parentDivRequestCreate.innerHTML = requestInputForm;
 
-  // Request Edit
-  var parentDivRequestEdit = document.getElementById("requestEditForm");
-  parentDivRequestEdit.innerHTML = requestInputForm;
+    // Request Edit
+    var parentDivRequestEdit = document.getElementById("requestEditForm");
+    parentDivRequestEdit.innerHTML = requestInputForm;
 
   } // End function gotData
 }
