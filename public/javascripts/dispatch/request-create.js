7f31f510769a75ec710745335747f4ce124965ff
NEH <erik@bodigiti.com>
2017-07-06 16:55:30 -0400

    Requests Create + Edit: both forms Working Correctly -> 1) Created two full forms via HTML. 2) Each of the two forms contain IDs that are unique to the form: createVendor, editVendor, etc.

--- public/javascripts/dispatch/request-create.js
+++ public/javascripts/dispatch/request-create.js
@@ -8,7 +8,7 @@ function submitRequestCreateForm(){
 
   // Cookie/Token authentication
   var authToken = Cookies.get('fb-auth-token');
-  console.log("create-request.hbs auth token ", authToken);
+//  console.log("create-request.hbs auth token ", authToken);
   firebase.auth().signInWithCustomToken(authToken)
   .then(function() {
 
@@ -20,13 +20,14 @@ function submitRequestCreateForm(){
     /***************************************************************************
      * Form Element Options and Values
     ***************************************************************************/
-    let Vendor = document.getElementById('vendor').value;
-    let BranchFrom = document.getElementById('branch-from').value;
-    let BranchTo = document.getElementById('branch-to').value;
-    let Urgency = document.getElementById('urgency').value;
-    let Reference = document.getElementById('reference').value;
-    let Instructions = document.getElementById('instructions').value;
-    let Driver = document.getElementById('driver').value;
+    let Vendor = document.getElementById('createVendor').value;
+    console.log("Got Value? ", Vendor);
+    let BranchFrom = document.getElementById('createBranchFrom').value;
+    let BranchTo = document.getElementById('createBranchTo').value;
+    let Urgency = document.getElementById('createUrgency').value;
+    let Reference = document.getElementById('createReference').value;
+    let Instructions = document.getElementById('createInstructions').value;
+    let Driver = document.getElementById('createDriver').value;
     // Status of Dispatch Request
       if (Driver === "") {
         var Status = "requested";
--- public/javascripts/dispatch/request-edit.js
+++ public/javascripts/dispatch/request-edit.js
@@ -4,6 +4,7 @@
 //////////////////////////////////////////////////////////////////////////////
 function requestEditFormDataLoad(clicked_id) {
   var key = clicked_id; // Get key passed to URL from 'view' link
+  console.log("Got Key? ", key);
   // Use "KEY" url parameter to target the core/ record
   var dbRefEdit = firebase.database().ref().child('dispatch/' + key);
   // Re: "once" https://firebase.google.com/docs/reference/js/firebase.database.Query#once
@@ -16,92 +17,17 @@ function requestEditFormDataLoad(clicked_id) {
     // assign above core data to 'coreRecord'
     // data.val() returns Object; destructure to pull out individual property values
     var dispatchRecord = data.val();
-
     // Destructure Object to individual property values
     var { BranchFrom, BranchTo, Driver, Instructions, Reference, Urgency, Vendor } = dispatchRecord;
+
     if (Driver !== "") {
-      document.getElementById("Driver").value = Driver;
+      document.getElementById("editDriver").value = Driver;
     }
-    document.getElementById("BranchFrom").value = BranchFrom;
-    document.getElementById("BranchTo").value = BranchTo;
-    document.getElementById("Instructions").value = Instructions;
-    document.getElementById("Reference").value = Reference;
-    document.getElementById("Urgency").value = Urgency;
-    document.getElementById("Vendor").value = Vendor;
+    document.getElementById("editBranchFrom").value = BranchFrom;
+    document.getElementById("editBranchTo").value = BranchTo;
+    document.getElementById("editInstructions").value = Instructions;
+    document.getElementById("editReference").value = Reference;
+    document.getElementById("editUrgency").value = Urgency;
+    document.getElementById("editVendor").value = Vendor;
   } // End function gotData
 }
-
-/*******************************************************************************
- * Post form Data to Database
-*******************************************************************************/
-/*
-function submitRequestEditForm(){
-  event.preventDefault();
-
-  // Cookie/Token authentication
-  var authToken = Cookies.get('fb-auth-token');
-  console.log("create-request.hbs auth token ", authToken);
-  firebase.auth().signInWithCustomToken(authToken)
-  .then(function() {
-
-    // Server Date/Time https://firebase.google.com/docs/reference/js/firebase.database.ServerValue
-    const DateTimeStampServer = firebase.database.ServerValue.TIMESTAMP;
-    let Date = DateTimeStampServer;
-    Date = moment().format('L'); // Format date with moment.js
-    let DateTime = moment().format('h:mm:ss A');
-    ////////////////////////////////////////////////////////////////////////////
-    // Form Element Options and Values
-    ////////////////////////////////////////////////////////////////////////////
-    let Vendor = document.getElementById('vendor').value;
-    let BranchFrom = document.getElementById('branch-from').value;
-    let BranchTo = document.getElementById('branch-to').value;
-    let Urgency = document.getElementById('urgency').value;
-    let Reference = document.getElementById('reference').value;
-    let Instructions = document.getElementById('instructions').value;
-    let Driver = document.getElementById('driver').value;
-    // Status of Dispatch Request
-      if (Driver === "") {
-        var Status = "requested";
-      } else {
-        Status = "dispatched";
-      }
-
-    // Data to submit to database
-    var submitData = {
-      DateTimeStampServer,
-      Date,
-      DateTime,
-      Vendor,
-      BranchFrom,
-      BranchTo,
-      Urgency,
-      Reference,
-      Instructions,
-      Driver,
-      Status
-    }
-
-    // Get a key for a new core Record
-    var newKey = firebase.database().ref().child('dispatch').push().key;
-
-    // write the new core data to the core list
-    var updates = {};
-    updates['/dispatch/' + newKey] = submitData;
-
-    // update the new-key-record with the data
-    var dbUpdate = firebase.database().ref().update(updates);
-
-    // Close Reveal Modal (Foundation)
-    // http://foundation.zurb.com/sites/docs/reveal.html
-    $('#requestCreate').foundation('close');
-
-  })
-  .catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    console.log(errorCode);
-    var errorMessage = error.message;
-    console.log(errorMessage);
-  });
-}; // End submitCoreForm Function
-*/
