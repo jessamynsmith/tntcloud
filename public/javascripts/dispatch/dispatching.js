4c29233ce1c39acdfcb8803c95b621ea95020b40
NEH <erik@bodigiti.com>
2017-07-11 22:15:32 -0400

    dispatching.js + Current Logged In UID: if Current UID IS NOT EQUAL TO THE DISPATCH RECORD CreatedByUID ---THEN--- HIDE THE CreatedByUID.

--- public/javascripts/dispatch/dispatching.js
+++ public/javascripts/dispatch/dispatching.js
@@ -13,7 +13,7 @@ if (isAdmin) {
 /*******************************************************************************
 * Handlebars
 *******************************************************************************/
-// Handlebars #if ../isAdmin :: https://stackoverflow.com/questions/13645084/access-a-variable-outside-the-scope-of-a-handlebars-js-each-loop
+// #if ../isAdmin === https://stackoverflow.com/questions/13645084/access-a-variable-outside-the-scope-of-a-handlebars-js-each-loop
 var rawTemplateRequestList =
 `{{#each dispatch}}
   <div class="data-row row expanded small-12 medium-6 large-6 columns tnt-card-output">
@@ -28,6 +28,7 @@ var rawTemplateRequestList =
       <div>Reference:&nbsp;{{Reference}}</div>
       <div>Instructions:&nbsp;{{Instructions}}</div>
       <div>Driver:&nbsp;{{Driver}}</div>
+      <div class="createdByUID">{{ CreatedByUID }}</div>
     </div>
         <div class="footer">
         <div style="float: left;">
@@ -43,28 +44,6 @@ var rawTemplateRequestList =
 {{/each}}`;
 
 /*******************************************************************************
- * Handlebars Compile + Render
-*******************************************************************************/
-// FYI - Node.js is needed if I want to pre-compile templates
-var compiledTemplate = Handlebars.compile(rawTemplateRequestList);
-
-function handleData(parentSelector, gotData) {
-  var parentDiv = $(parentSelector);
-  // clear the records so when value is updated new records are displayed (see bottom of code)
-  parentDiv.empty();
-  // assign above data to 'dataVal'
-  var dataVal = gotData.val();
-  // pass the data to the handlebars template (up above)
-  // about the '../' in the above {{# if ../ }}
-  // required when using handlebars template in separate .js file outside of .hbs node express
-  // 'showDelete' is coming from top of this file, not the dispatch.js route file
-  var data = { showDeleteLink: showDelete, dispatch: dataVal };
-  var html = compiledTemplate(data);
-
-  parentDiv.append(html);
-}
-
-/*******************************************************************************
  * Dispatch Data
 *******************************************************************************/
 // Listen for Child Events: https://firebase.google.com/docs/database/web/lists-of-data#listen_for_child_events
@@ -74,8 +53,6 @@ function handleData(parentSelector, gotData) {
 var authToken = Cookies.get('fb-auth-token');
 firebase.auth().signInWithCustomToken(authToken)
 .then(function() {
-  // Cookie/Token authentication
-  var authToken = Cookies.get('fb-auth-token');
 
   // Requested Dispatch Items
   var dbRefRequested = firebase.database().ref().child('dispatch').orderByChild('Status').equalTo('requested');
@@ -92,3 +69,40 @@ firebase.auth().signInWithCustomToken(authToken)
   });
 
 });
+
+
+/*******************************************************************************
+ * Handlebars Compile + Render
+*******************************************************************************/
+// FYI - Node.js is needed if I want to pre-compile templates
+var compiledTemplate = Handlebars.compile(rawTemplateRequestList);
+
+function handleData(parentSelector, gotData) {
+
+  var parentDiv = $(parentSelector);
+  // clear the records so when value is updated new records are displayed (see bottom of code)
+  parentDiv.empty();
+  // assign above data to 'dataVal'
+  var dataVal = gotData.val();
+  // pass the data to the handlebars template (up above)
+  // about the '../' in the above {{# if ../ }}
+  // required when using handlebars template in separate .js file outside of .hbs node express
+  // 'showDelete' is coming from top of this file, not the dispatch.js route file
+  var data = { showDeleteLink: showDelete, dispatch: dataVal };
+  var html = compiledTemplate(data);
+
+  parentDiv.append(html);
+
+
+  var currentUID = firebase.auth().currentUser.uid;
+  console.log("currentUID dispatching.js Here! ", currentUID);
+
+  var recordUID = document.getElementsByClassName("createdByUID");
+  console.log("Got recordUID array? ", recordUID);
+  for (var i = 0; i < recordUID.length; i++) {
+    if (currentUID !== recordUID[i].innerText) {
+      recordUID[i].className += " hide";
+      console.log("array for output ", recordUID[i].className);
+    }
+  }
+}
