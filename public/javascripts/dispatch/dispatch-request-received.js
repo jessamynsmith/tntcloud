25dd8e878c9e7e00a1038d64ec37afd0c4dd4cd7
NEH <erik@bodigiti.com>
2017-07-14 14:13:01 -0400

    Dispatch + Public Files: Move the Individual 'Branch' files into new folder 'Branch' and update the JS file references in the dispatching.hbs file.

---
+++ public/javascripts/dispatch/dispatch-request-received.js
@@ -0,0 +1,53 @@
+
+////////////////////////////////////////////////////////////////////////////////
+// Delete Confirm Form Load
+////////////////////////////////////////////////////////////////////////////////
+// function called onclick in dispatching.js
+function dispatchDeleteLoadConfirmForm(clicked_id) {
+  // fyi - for some reason, above global variable 'editKey' is not available in this function
+  var key = clicked_id;
+
+  var buttonHTML =
+  `<button
+      onClick="dispatchDelete(this.id);" id="${key}" class="button alert" type="button">Confirm Delete
+  </button>`;
+  // get div id deleteButtonParent and add buttonHTML as innerHTML
+  var parentDiv = document.getElementById("deleteButtonParent");
+  parentDiv.innerHTML = buttonHTML;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Delete Record
+////////////////////////////////////////////////////////////////////////////////
+// function called onclick in dispatching.hbs via -> buttonHTML in above function dispatchDeleteLoadConfirmForm
+function dispatchDelete(clicked_id) {
+  // fyi - for some reason, above global variable 'editKey' is not available in this function
+  var key = clicked_id;
+
+  // Cookie/Token Authentication: Sign In with Auth Token so can pull Firebase Data from Front-end
+  var authToken = Cookies.get('fb-auth-token');
+  firebase.auth().signInWithCustomToken(authToken)
+  .then(function() {
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Delete Record
+    ////////////////////////////////////////////////////////////////////////////
+    // locate the record to delete
+    var dbRefDelete = firebase.database().ref().child('dispatch/' + key);
+    // update data
+    dbRefDelete.remove();
+
+    // Close Reveal Modal (Foundation)
+    // http://foundation.zurb.com/sites/docs/reveal.html
+    $('#requestDelete').foundation('close');
+
+  })
+  .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    console.log(errorCode);
+    var errorMessage = error.message;
+    console.log(errorMessage);
+  });
+}; // End dispatchEditFormDataUpdate Function
