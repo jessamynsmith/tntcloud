6f5134e1ea9885c47bd57f2f8e7fbb4c2c538f97
NEH <erik@bodigiti.com>
2017-07-16 10:55:07 -0400

    Core-Warranty PEOPLE PAGE + People List: Add Sort by Alphabetical (using original js code from previous version of the app.

---
+++ public/javascripts/core-warranty/people-list.js
@@ -0,0 +1,33 @@
+
+/***************************************************************************
+ * Sort Table Rows
+ **************************************************************************/
+var myArray = [];
+var listElements = document.getElementsByClassName("data-row");
+console.log("List Elements ", listElements);
+
+for (var i = 0; i < listElements.length; i++) {
+  trINNER = listElements[i].innerHTML;
+  myArray.push(trINNER);
+}
+
+myArray.sort(function(x,y){
+  var a = String(x);
+  var b = String(y);
+  if (a > b)     {
+     return 1
+  }    else  if (a < b) {
+     return -1
+  }    else    {
+     return 0;
+  }
+});
+
+for (var i = 0; i < myArray.length; i++) {
+  var trNext = document.createElement('tr');
+  trNext.innerHTML = myArray[i];
+
+  document.getElementById("new-people-list").appendChild(trNext);
+}
+
+document.getElementById("people-list").remove();
